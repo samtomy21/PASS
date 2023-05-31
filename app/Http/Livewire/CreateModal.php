dff91ffb8cce17b50d8a7a283fac8b77dbb87b8f
Yury B. Huaquilla T <yhuaquillat@est.unap.edu.pe>
2023-05-31 12:09:10 -0500

    hola

--- app/Http/Livewire/CreateModal.php
+++ app/Http/Livewire/CreateModal.php
@@ -9,18 +9,22 @@ use Carbon\Carbon;
 
 use App\Models\Pass;
 
+use App\Models\Charge;
+use App\Models\Dependence;
+use App\Models\User;
+
 class CreateModal extends Component
 {
     public $open = false;
     public function render(Request $request, Pass $pass)
     {
         $date = Carbon::now();
-        // $times = Time::all();
+        // $charges = Charge::all();
         // $dependences = Dependence::all();
         // $users = User::all();
         //return dd($date);
 
         // return view('livewire.create-modal', compact('date', 'pass', 'charges', 'dependences', 'users'));
-        return view('livewire.create-modal', compact('date'));
+        return view('livewire.create-modal', compact('date','times'));
     }
 }
