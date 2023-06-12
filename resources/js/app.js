fb707e21a1a96d06a32dadf9e78ce26a58307c80
Brad T. Sanchez M <tylerxx485@gmail.com>
2023-06-12 14:49:31 -0500

    estados y pdf ver

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,8 @@ import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 window.Alpine = Alpine;
 
+// window.Swal = require('sweetalert2');
+
 Alpine.plugin(focus);
 
 Alpine.start();
--- resources/views/userchecks/index.blade.php
+++ resources/views/userchecks/index.blade.php
@@ -38,19 +38,19 @@
                             <th scope="col" class="px-2 py-2">
                                 <input type="checkbox" name="" id="select_all_ids" class="rounded">
                             </th>
-                            <th scope="col" class="px-4 py-3">Id</th>
-                            <th scope="col" class="px-4 py-3">N° Targeta</th>
-                            <th scope="col" class="px-4 py-3">Nombre</th>
-                            <th scope="col" class="px-4 py-3">Cargo</th>
-                            <th scope="col" class="px-4 py-3">Dependencia</th>
-                            <th scope="col" class="px-4 py-3">Motivo</th>
-                            <th scope="col" class="px-4 py-3">Lugar</th>
-                            <th scope="col" class="px-4 py-3">Tiempo Autorizado</th>
-                            <th scope="col" class="px-4 py-3">Hora de Salida</th>
-                            <th scope="col" class="px-4 py-3">Hora de Llegada</th>
-                            <th scope="col" class="px-4 py-3">Fecha</th>
-                            <th scope="col" class="px-4 py-3">Estado</th>
-                            <th scope="col" class="px-4 py-3 border-slate-200">Opciones</th>
+                            <th scope="col" class="px-1 py-2">Id</th>
+                            <th scope="col" class="px-1 py-2">N° Targeta</th>
+                            <th scope="col" class="px-1 py-2">Nombre</th>
+                            <th scope="col" class="px-1 py-2">Cargo</th>
+                            <th scope="col" class="px-1 py-2">Dependencia</th>
+                            <th scope="col" class="px-1 py-2">Motivo</th>
+                            <th scope="col" class="px-1 py-2">Lugar</th>
+                            <th scope="col" class="px-1 py-2">Tiempo Autorizado</th>
+                            <th scope="col" class="px-1 py-2">Hora de Salida</th>
+                            <th scope="col" class="px-1 py-2">Hora de Llegada</th>
+                            <th scope="col" class="px-1 py-2">Fecha</th>
+                            <th scope="col" class="px-1 py-2">Estado</th>
+                            <th scope="col" class="px-1 py-2">Opciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -62,33 +62,45 @@
                             <td class="px-2 py-2">
                                 <input type="checkbox" class="checkbox_ids rounded" name="ids" value="{{ $pass->id }}">
                             </td>
-                            <td scope="row" class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap">{{ $pass->id }}</td>
-                            <td class="px-6 py-4">{{ $pass->user->ncard }}</td>
-                            <td class="px-6 py-4">{{ $pass->user->name }}</td>
-                            <td class="px-6 py-4">{{ $pass->user->charge->name_charge }}</td>
-                            <td class="px-6 py-4">{{ $pass->user->dependence->name_dependence }}</td>
-                            <td class="px-6 py-4">{{ $pass->motive }}</td>
-                            <td class="px-6 py-4">{{ $pass->place }}</td>
-                            <td class="px-6 py-4">{{ $pass->time->time_permision }}</td>
-                            <td class="px-6 py-4">{{ $pass->input }}</td>
-                            <td class="px-6 py-4">{{ $pass->output }}</td>
-                            <td class="px-6 py-4">{{ $pass->date }}</td>
-                            <td class="px-6 py-4">{{ $pass->estado }}</td>
+                            <td scope="row" class="px-2 font-medium text-gray-700 whitespace-nowrap">{{ $pass->id }}</td>
+                            <td class="px-2">{{ $pass->user->ncard }}</td>
+                            <td class="px-2">{{ $pass->user->name }}</td>
+                            <td class="px-2">{{ $pass->user->charge->name_charge }}</td>
+                            <td class="px-2">{{ $pass->user->dependence->name_dependence }}</td>
+                            <td class="px-2">{{ $pass->motive }}</td>
+                            <td class="px-2">{{ $pass->place }}</td>
+                            <td class="px-2">{{ $pass->time->time_permision }}</td>
+                            <td class="px-2">{{ $pass->input }}</td>
+                            <td class="px-2">{{ $pass->output }}</td>
+                            <td class="px-2">{{ $pass->date }}</td>
+                            <td class="px-2">
+                                <div class=" flex justify-center items-center">
+                                    @if ($pass->estado === 3)
+                                        <span class="inline-block h-4 w-4 rounded-full bg-green-500"></span>
+                                    @elseif ($pass->estado === 2)
+                                        <span class="inline-block h-4 w-4 rounded-full bg-blue-500"></span>
+                                    @elseif ($pass->estado === 1)
+                                        <span class="inline-block h-4 w-4 rounded-full bg-yellow-500"></span>
+                                    @elseif ($pass->estado === 0)
+                                        <span class="inline-block h-4 w-4 rounded-full bg-red-500"></span>
+                                    @endif
+                                </div>
+                            </td>
                             <!-- <td class="px-6 py-4">{{ $pass->observation }}</td> -->
-                            <td class="flex px-auto py-4 mb-2 items-center">
-                                <a href="{{ route('passes.firmar', $pass) }}" class="bg-sky-900 text-white rounded px-2 py-1 mx-1">Firmar</a>
+                            <td class="flex px-auto py-5 mb-2 justify-center items-center flex-col text-center md:flex-row">
+                                <a href="{{ route('passes.firmar', $pass) }}" class="bg-sky-900 text-white rounded px-2 py-1 mx-1 my-auto md:mt-3 mb-3">Firmar</a>
                                 @livewire('edit-modal', ['pass' => $pass], key($pass->id))
                                 <form action="{{ route('passes.destroy', $pass) }}" method="POST">  <!--onsubmit="return confirm('{{ trans('Estas seguro que desea eliminar? ') }}'); "> -->
                                     @csrf
                                     @method('DELETE')
-                                    <input type="submit" class="bg-red-500 text-white rounded px-2 py-1 mx-1" value="Eliminar">
+                                    <input type="submit" class="bg-red-500 text-gray-50 p-1 rounded mx-1" value="Eliminar">
                                 </form>
                             </td>
 
                         </tr>
                         @empty
                         <tr class="bg-white border-b bg-white-800 dark:border-gray-700">
-                            <td colspan="13">No has creado ningun permiso</td>
+                            <td colspan="13">No se tiene ningun permiso</td>
                         </tr>
                         @endforelse
                     </tbody>
