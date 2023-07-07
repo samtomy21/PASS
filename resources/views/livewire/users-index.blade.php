7bf14e39990efeba73aeadb728ffd2a2ddc9f84e
Brad T. Sanchez M <tylerxx485@gmail.com>
2023-07-07 10:14:56 -0500

    30: iconos

--- resources/views/livewire/users-index.blade.php
+++ resources/views/livewire/users-index.blade.php
@@ -1,5 +1,5 @@
 <div>
-    
+
     <div>
         <!-- search -->
         <div class="m-2">
@@ -7,45 +7,50 @@
         </div>
 
         @if($users->count())
-            <!-- table data -->
-            <div class="relative overflow-x-auto bg-white rounded-lg">
-                <table class="table-fixed min-w-full text-sm text-left text-gray-800">
-                    <thead class="text-xs uppercase bg-gray-700 text-white">
-                        <tr class="align-center">
-                            <th scope="col" class="px-2 py-2">ID</th>
-                            <th scope="col" class="px-1 py-2">Nombre</th>
-                            <th scope="col" class="px-1 py-2">ncard</th>
-                            <th scope="col" class="px-1 py-2">Email</th>
-                            <th scope="col" class="px-1 py-2">Opciones</th>
-                        </tr>
-                    </thead>
+        <!-- table data -->
+        <div class="relative overflow-x-auto bg-white rounded-lg">
+            <table class="table-fixed min-w-full text-sm text-left text-gray-800">
+                <thead class="text-xs uppercase bg-gray-700 text-white">
+                    <tr class="align-center">
+                        <th scope="col" class="px-2 py-2">ID</th>
+                        <th scope="col" class="px-1 py-2">Nombre</th>
+                        <th scope="col" class="px-1 py-2">ncard</th>
+                        <th scope="col" class="px-1 py-2">Email</th>
+                        <th scope="col" class="px-1 py-2">Opciones</th>
+                    </tr>
+                </thead>
 
-                    <tbody>
-                        @foreach ($users as $user)
-                            <tr>
-                                <td scope="col" class="px-2 py-2">{{ $user->id }}</td>
-                                <td scope="col" class="px-1 py-2">{{ $user->name }}</td>
-                                <td scope="col" class="px-1 py-2">{{ $user->ncard }}</td>
-                                <td scope="col" class="px-1 py-2">{{ $user->email }}</td>
-                                <td scope="col" class="px-1 py-2">
-                                    <a href="{{ route('users.edit', $user) }}" class="bg-yellow-500 text-white rounded px-2 py-1 mx-1">Editar</a>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <!-- paginate -->
-            <div class="opacity-80 h-px my-2" style="background: linear-gradient(to right, rgba(200, 200, 200, 0) 0%, rgba(200, 200, 200, 1) 30%, rgba(200, 200, 200, 1) 70%, rgba(200, 200, 200, 0) 100%);"></div>
-            <div class="px-3">
-                {{ $users->links() }}
-            </div>
+                <tbody>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td scope="col" class="px-2 py-2">{{ $user->id }}</td>
+                        <td scope="col" class="px-1 py-2">{{ $user->name }}</td>
+                        <td scope="col" class="px-1 py-2">{{ $user->ncard }}</td>
+                        <td scope="col" class="px-1 py-2">{{ $user->email }}</td>
+                        <td scope="col" class="px-1 py-2">
+                            <a href="{{ route('users.edit', $user) }}" class="bg-yellow-500 text-white rounded px-1 py-1 mx-auto flex items-center justify-center">
+                            <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
+                                <path d="M21.731 2.269a2.625 2.625 0 00-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 000-3.712zM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 00-1.32 2.214l-.8 2.685a.75.75 0 00.933.933l2.685-.8a5.25 5.25 0 002.214-1.32l8.4-8.4z" />
+                                <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
+                            </svg>
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <!-- paginate -->
+        <div class="opacity-80 h-px my-2" style="background: linear-gradient(to right, rgba(200, 200, 200, 0) 0%, rgba(200, 200, 200, 1) 30%, rgba(200, 200, 200, 1) 70%, rgba(200, 200, 200, 0) 100%);"></div>
+        <div class="px-3">
+            {{ $users->links() }}
+        </div>
 
         @else
 
-            <div>
-                <strong>No existen Registros</strong>
-            </div>
+        <div>
+            <strong>No existen Registros</strong>
+        </div>
 
         @endif
     </div>
--- resources/views/passes/index.blade.php
+++ resources/views/passes/index.blade.php
@@ -40,6 +40,7 @@
                             <th scope="col" class="px-1 py-2">Tiempo Autorizado</th>
                             <th scope="col" class="px-1 py-2">Fecha</th>
                             <th scope="col" class="px-1 py-2">Estado</th>
+                            <th scope="col" class="px-1 py-2">Opciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -68,14 +69,22 @@
                                 </div>
                             </td>
                             <!-- <td class="px-6 py-4">{{ $pass->observation }}</td> -->
-                            <td class="flex px-auto py-5 mb-2 justify-center items-center flex-col text-center md:flex-row">
-                                <a href="{{ route('passes.firmar', $pass) }}" class="bg-sky-900 text-white rounded px-2 py-1 mx-1 my-auto md:mt-3 mb-3">Firmar</a>
+                            <td class="flex px-auto py-1 mb-2 justify-center items-center flex-col text-center md:flex-row">
+                                <a href="{{ route('passes.firmar', $pass) }}" class="bg-sky-900 text-white rounded px-2 py-1 mx-1 my-auto md:mt-3 mb-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                    <path fill-rule="evenodd" d="M12 3.75a6.715 6.715 0 00-3.722 1.118.75.75 0 11-.828-1.25 8.25 8.25 0 0112.8 6.883c0 3.014-.574 5.897-1.62 8.543a.75.75 0 01-1.395-.551A21.69 21.69 0 0018.75 10.5 6.75 6.75 0 0012 3.75zM6.157 5.739a.75.75 0 01.21 1.04A6.715 6.715 0 005.25 10.5c0 1.613-.463 3.12-1.265 4.393a.75.75 0 01-1.27-.8A6.715 6.715 0 003.75 10.5c0-1.68.503-3.246 1.367-4.55a.75.75 0 011.04-.211zM12 7.5a3 3 0 00-3 3c0 3.1-1.176 5.927-3.105 8.056a.75.75 0 11-1.112-1.008A10.459 10.459 0 007.5 10.5a4.5 4.5 0 119 0c0 .547-.022 1.09-.067 1.626a.75.75 0 01-1.495-.123c.041-.495.062-.996.062-1.503a3 3 0 00-3-3zm0 2.25a.75.75 0 01.75.75A15.69 15.69 0 018.97 20.738a.75.75 0 01-1.14-.975A14.19 14.19 0 0011.25 10.5a.75.75 0 01.75-.75zm3.239 5.183a.75.75 0 01.515.927 19.415 19.415 0 01-2.585 5.544.75.75 0 11-1.243-.84 17.912 17.912 0 002.386-5.116.75.75 0 01.927-.515z" clip-rule="evenodd" />
+                                </svg>
+                                </a>
                                 @livewire('edit-modal', ['pass' => $pass], key($pass->id))
                                 <form action="{{ route('passes.destroy', $pass) }}" method="POST"> <!-- onsubmit=" return confirm('{{ trans('Estas seguro que desea eliminar? ') }}') "> -->
                                     @csrf
                                     @method('DELETE')
                                     <!-- <input type="submit" class="bg-red-500 text-white rounded px-2 py-1 mx-1 my-3 md:mt-3" value="Eliminar"> -->
-                                    <button type="submit" class="bg-red-500 text-gray-50 p-1 rounded mx-1">Eliminar</button>
+                                    <button type="submit" class="bg-red-500 text-gray-50 p-1 rounded mx-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                        <path fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
+                                    </svg>
+                                    </button>
                                 </form>
                             </td>
 
